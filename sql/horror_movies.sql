e2a29e67b7a3d5124456fff5533d74475ed8219a
Thomas Hauber <thomashauber01@gmail.com>
2024-05-30 14:36:03 -0500

    Commit and pushed horror movie assignment.

--- sql/horror_movies.sql
+++ sql/horror_movies.sql
@@ -1,4 +1,4 @@
-SELECT id, name, imdb_rating
+SELECT id AS 'Movie_ID', name AS 'Movie_Title', imdb_rating AS 'Rating'
 FROM movies
 WHERE genre = 'horror' and year <= 1985
 ORDER BY imdb_rating DESC
